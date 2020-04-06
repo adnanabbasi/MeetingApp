c6c38db9d41812213af1dd8052983a1ecb9c9d78
Adnan Abbasi <adnanabbasi@Adnans-iMac.local>
2020-04-05 22:11:59 -0400

    End of Section 13

--- MeetingApp-SPA/src/app/app.module.ts
+++ MeetingApp-SPA/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
 import { FileUploadModule } from 'ng2-file-upload';
+import { TimeagoModule } from 'ngx-timeago';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -58,6 +59,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
    ],
    imports: [
       BrowserModule,
+      TimeagoModule.forRoot(),
       BrowserAnimationsModule,
       HttpClientModule,
       FormsModule,
